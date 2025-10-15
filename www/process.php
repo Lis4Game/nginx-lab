f530e2600fb50e4c59909599da635ce075e63841
Lis4Game <rusakov_vladislav_2005@mail.ru>
2025-10-15 20:11:46 +0200

    Обновлён файл process.php (добавил код)

---
+++ www/process.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$name = htmlspecialchars($_POST['name'] ?? '');
+$age = htmlspecialchars($_POST['age'] ?? '');
+
+$_SESSION['name'] = $name;
+$_SESSION['age'] = $age;
+
+header("Location: index.php");
+exit();
+
+?>
