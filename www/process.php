eb6901c3be58596b0ab344b4647520bd47bb4ddd
Lis4Game <rusakov_vladislav_2005@mail.ru>
2025-10-15 20:43:06 +0200

    Обновил файлы: form.html (изменил макс. значение для проверки); index.php (добавил вывод ошибки); process.php (добавил обработчик введённых данных)

--- www/process.php
+++ www/process.php
@@ -4,6 +4,21 @@ session_start();
 $name = htmlspecialchars($_POST['name'] ?? '');
 $age = htmlspecialchars($_POST['age'] ?? '');
 
+$errors = [];
+if(empty($name)) $errors[] = "Имя не может быть пустым";
+if(empty($age)) $errors[] = "Возраст не указан";
+elseif(!is_numeric($age)) $errors[] = "Некорректно указан возраст";
+else {
+	if($age<1||$age>120) $errors[] = "Укажите настояций возраст (1-120)";
+}
+
+if(!empty($errors)){
+    $_SESSION['errors'] = $errors;
+    header("Location: index.php");
+    exit();
+}
+
+
 $_SESSION['name'] = $name;
 $_SESSION['age'] = $age;
 
