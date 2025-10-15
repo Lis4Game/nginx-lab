6c21741464fa3749d64baa99eb65d6cb59c6cd37
Lis4Game <rusakov_vladislav_2005@mail.ru>
2025-10-15 20:24:09 +0200

    Обновил код в файле process.php и добавился файл data.txt с информацией с формы (2-ой лабы)

--- www/process.php
+++ www/process.php
@@ -8,6 +8,11 @@ $_SESSION['name'] = $name;
 $_SESSION['age'] = $age;
 
 header("Location: index.php");
+
+$line = $name . ";" . $age . "\n"; //<--- строка для записи в файл
+
+file_put_contents("data.txt", $line, FILE_APPEND);
+
 exit();
 
 ?>
