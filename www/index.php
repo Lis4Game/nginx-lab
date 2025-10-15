a27e8cfa8c37ca4ebb23179cc277e86eb3d6786d
Lis4Game <rusakov_vladislav_2005@mail.ru>
2025-10-15 20:15:28 +0200

    Обновлён файл Index.php (Добавлен вывод данных из сессии на index.php)

--- www/index.php
+++ www/index.php
@@ -1,2 +1,14 @@
-<?php
-phpinfo();
+<?php session_start(); ?>
+
+<?php if(isset($_SESSION['name'])): ?>
+    <p>Данные из сессии:</p>
+    <ul>
+        <li>Имя: <?= $_SESSION['name'] ?></li>
+        <li>Email: <?= $_SESSION['age'] ?></li>
+    </ul>
+<?php else: ?>
+    <p>Данных пока нет.</p>
+<?php endif; ?>
+
+<a href="form.html">Заполнить форму</a> |
+<a href="view.php">Посмотреть все данные</a>
