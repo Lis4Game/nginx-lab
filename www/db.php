6dcac020babd621ebfabfb8c83d5829eed24cbf2
Lis4Game <rusakov_vladislav_2005@mail.ru>
2025-12-12 20:03:56 +0200

    Изменил файлы под мою тему, также добавил файл send_form

--- www/db.php
+++ www/db.php
@@ -1,14 +1,15 @@
 <?php
 $host = 'db';
-$db   = 'lab5_db';
-$user = 'lab5_user';
-$pass = 'lab5_pass';
+$db   = 'lab7_db';
+$user = 'lab7_user';
+$pass = 'lab7_pass';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
